9dc0e775bb62b2715e80ee42afab5fbe26354b3c
kieron <kieron.wray@gmail.com>
2020-05-17 14:03:10 +0100

    bufixed somejump bugs

--- training1/src/entities/hero.js
+++ training1/src/entities/hero.js
@@ -3,7 +3,7 @@
 import Phaser from 'phaser';
 
 
-const MAX_V = { x:250, y:400};
+const MAX_V = { x: 250, y: 400 };
 const ACCELERATION_X = MAX_V.x * 4;   // reach max speed in 1/4 of a second
 const JUMP_SPEED = MAX_V.y * 2.5;
 
@@ -25,7 +25,7 @@ class Hero extends Phaser.GameObjects.Sprite {
 
         this.body.setDragX(ACCELERATION_X * 0.75);
 
-        this.body.setMaxVelocity (MAX_V.x, MAX_V.y);
+        this.body.setMaxVelocity(MAX_V.x, MAX_V.y);
         this.keys = scene.cursorKeys;
 
     }
@@ -48,20 +48,31 @@ class Hero extends Phaser.GameObjects.Sprite {
 
         const didPressJump = Phaser.Input.Keyboard.JustDown(this.keys.up);
 
-        if (didPressJump ) {
+        // reset doublejump when we hit the floor (note, this ignores platforms/enemies/etc.)
+        if (this.body.onFloor()) {
+            this.canDoubleJump = false;
+        }
+
+        if (this.body.velocity.y > 0) {
+            this.isJumping = false;
+        }
+
+        if (didPressJump) {
             // single jump if on the floor
             if (this.body.onFloor()) {
-                this.canDoubleJump=true;
+                this.canDoubleJump = true;
+                this.isJumping = true;
                 this.body.setVelocityY(-JUMP_SPEED);
             } else if (this.canDoubleJump) {
                 // doublejump
                 this.body.setVelocityY(-JUMP_SPEED * 1.75);
+                this.isJumping = true;
                 this.canDoubleJump = false;
             }
         }
 
-        if (!this.keys.up.isDown && this.body.velocity.y < -150) {
-            this.body.setVelocityY (-150);
+        if (!this.keys.up.isDown && this.body.velocity.y < -150 && this.isJumping) {
+            this.body.setVelocityY(-150);
         }
     }
 }
